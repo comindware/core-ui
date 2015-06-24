937de26816c1f47a40b57c383b31b00e8ebbd30e
Oleg Verevkin <oleg.verevkin@comindware.com>
2015-06-24 18:28:17 +0300

    bug/189477

--- form/editors/DropdownEditorView.js
+++ form/editors/DropdownEditorView.js
@@ -69,6 +69,8 @@ define([
                 this.listenTo(this.collection, 'remove', this.__onCollectionChange);
                 this.listenTo(this.collection, 'reset', this.__onCollectionChange);
 
+                this.listenTo(this, 'change', this.__onValueChange);
+
                 this.viewModel = new Backbone.Model({
                     button: new Backbone.Model({
                         value: this.__findModel(this.getValue()),
@@ -184,6 +186,10 @@ define([
                 if (triggerChange) {
                     this.__triggerChange();
                 }
+            },
+
+            __onValueChange: function () {
+                this.model.set('linkedObjectType', this.viewModel.get('button').get('value').get('id'));
             },
 
             onValueSet: function (o) {
