5424a50efb1d5d0ff422d36e4e533666999a61d7
Nikita Sushkov <Nikita.Sushkov@comindware.com>
2015-06-25 17:16:29 +0300

    expression control final fixes

---
+++ form/validators/regExpValidator.js
@@ -0,0 +1,12 @@
+﻿define(['module/lib'], function () {
+    'use strict';
+
+    var regExpOld = Backbone.Form.validators.regexp;
+
+    Backbone.Form.validators.regexp = function (options) {
+        return _.wrap(regExpOld(options), function (func, value) {
+            var val = _.isObject(value) ? value.value : value;
+            return func(val);
+        });
+    };
+});
