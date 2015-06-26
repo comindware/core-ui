58afc59fb7342578403bba253badba3a529e6bb6
Stepan Burguchev <Stepan.Burguchev@comindware.com>
2015-06-26 15:48:18 +0300

    application

---
+++ services/RoutingService.js
@@ -0,0 +1,45 @@
+/**
+ * Developer: Stepan Burguchev
+ * Date: 6/26/2015
+ * Copyright: 2009-2015 Comindware®
+ *       All Rights Reserved
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Comindware
+ *       The copyright notice above does not evidence any
+ *       actual or intended publication of such source code.
+ */
+
+/* global define, require, Handlebars, Backbone, Marionette, $, _, Localizer */
+
+define([],
+    function () {
+        'use strict';
+
+        return {
+            initialize: function () {
+
+                /*new AppRouter({
+                 controller:new Controller()
+                 });*/
+                /*var Controller = Backbone.Marionette.Controller.extend({
+                    initialize: function (options) {
+                        //App.headerRegion.show(new HeaderView());
+                    },
+                    //gets mapped to in AppRouter's appRoutes
+                    index:function () {
+                        //App.mainRegion.show(new WelcomeView());
+                    }
+                });
+
+                //--
+                var AppRouter = Marionette.AppRouter.extend({
+                    //"index" must be a method in AppRouter's controller
+                    appRoutes: {
+                        "": "index"
+                    }
+                });
+
+                //--*/
+            }
+        };
+    });
