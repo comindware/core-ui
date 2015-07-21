aab77ce011e67490d1d76199222ff943632e5858
Stepan Burguchev <Stepan.Burguchev@comindware.com>
2015-07-21 17:33:00 +0300

    hiding global regions

--- services/WindowService.js
+++ services/WindowService.js
@@ -21,6 +21,10 @@ define([
 
         var state = {};
 
+        var classes = {
+            HIDDEN: 'dev-hidden-application-region'
+        };
+
         return {
             initialize: function (options) {
                 utilsApi.helpers.ensureOption(options, 'fadingRegion');
@@ -29,6 +33,7 @@ define([
                 state.options = options;
                 state.fadingRegion = options.fadingRegion;
                 state.popupRegion = options.popupRegion;
+                state.ui = options.ui;
 
                 state.fadingPanelView = new FadingPanelView();
                 state.fadingRegion.show(state.fadingPanelView);
@@ -36,20 +41,24 @@ define([
 
             showPopup: function (view, options) {
                 this.fadeIn(options);
+                state.ui.popupRegion.removeClass(classes.HIDDEN);
                 state.popupRegion.show(view);
             },
 
             closePopup: function () {
                 this.fadeOut();
                 state.popupRegion.reset();
+                state.ui.popupRegion.addClass(classes.HIDDEN);
             },
 
             fadeIn: function (options) {
+                state.ui.fadingRegion.removeClass(classes.HIDDEN);
                 state.fadingPanelView.fadeIn(options);
             },
 
             fadeOut: function () {
                 state.fadingPanelView.fadeOut();
+                state.ui.fadingRegion.addClass(classes.HIDDEN);
             }
         };
     });
