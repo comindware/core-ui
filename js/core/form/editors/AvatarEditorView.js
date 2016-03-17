6465847039c813ae480d5d7280c4cf8a72283d6f
Oleg Verevkin <oleg.verevkin@comindware.com>
2016-03-17 18:52:33 +0300

    Avatar editor refix

--- js/core/form/editors/AvatarEditorView.js
+++ js/core/form/editors/AvatarEditorView.js
@@ -13,11 +13,9 @@ import BaseItemEditorView from './base/BaseItemEditorView';
 import template from './templates/avatarEditor.hbs';
 
 const defaultOptions = {
-    fullName: null,
     removable: true,
     autoUpload: false,
-    refreshPreviewAfterUpload: false,
-    controller: null
+    refreshPreviewAfterUpload: false
 };
 
 /**
@@ -59,13 +57,13 @@ Backbone.Form.editors.Avatar = BaseItemEditorView.extend({
     
     initialize(options) {
         if (options.schema) {
-            _.extend(this.options, defaultOptions, _.pick(options.schema, _.keys(defaultOptions)));
+            Object.assign(this.options, defaultOptions, options.schema);
         } else {
-            _.extend(this.options, defaultOptions, _.pick(options || {}, _.keys(defaultOptions)));
+            Object.assign(this.options, defaultOptions, options);
         }
         
         helpers.ensureOption(this.options, 'controller');
-        this.controller = this.getOption('controller');        
+        this.controller = this.getOption('controller');
         
         this.__removed = false;
         this.__previewURL = null;
