d4dd08f49720743b7eca840bd4d914f9425d42d7
Stepan Burguchev <stepan.burguchev@comindware.com>
2016-07-18 14:01:13 +0300

    added third-parties initialization

--- src/Bootstrapper.js
+++ src/Bootstrapper.js
@@ -9,11 +9,24 @@
 "use strict";
 
 import { helpers } from './utils/utilsApi';
+import { $ } from './libApi';
 import AjaxService from './services/AjaxService';
 import MessageService from './services/MessageService';
 import UserService from './services/UserService';
 import WindowService from './services/WindowService';
 import LocalizationService from './services/LocalizationService';
+
+let initializeThirdParties = function () {
+    $.fn.datetimepicker.dates[LocalizationService.langCode] = {
+        days: LocalizationService.get('CORE.FORMATS.DATETIME.DAYSFULL').split(','), //["Sunday", "Monday", ... ]
+        daysShort: LocalizationService.get('CORE.FORMATS.DATETIME.DAYSSHORT').split(','), //["Sun", "Mon", ... ],
+        daysMin: LocalizationService.get('CORE.FORMATS.DATETIME.DAYSSHORT').split(','),
+        months: LocalizationService.get('CORE.FORMATS.DATETIME.MONTHS').split(','), //["January", "February", ... ]
+        monthsShort: LocalizationService.get('CORE.FORMATS.DATETIME.MONTHSSHORT').split(','), //["Jan", "Feb", ... ]
+        today: LocalizationService.get('CORE.FORMATS.DATETIME.TODAY'),
+        meridiem: LocalizationService.get('CORE.FORMATS.DATETIME.MERIDIEM').split(',')
+    };
+};
 
 export default {
     initialize: function (options) {
@@ -32,5 +45,7 @@ export default {
                 LocalizationService.get('CORE.BOOTSTRAPPER.ERRORS.DEFAULT.DESCRIPTION'),
                 LocalizationService.get('CORE.BOOTSTRAPPER.ERRORS.DEFAULT.TITLE'));
         });
+
+        initializeThirdParties();
     }
 };
