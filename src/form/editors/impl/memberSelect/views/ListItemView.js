537775f143adedfbbae8790c2880e5531fb2c956
Ksenia <Ksenia.Kartvelishvili@comindware.com>
2016-10-20 15:39:19 +0300

    fix member select

--- src/form/editors/impl/memberSelect/views/ListItemView.js
+++ src/form/editors/impl/memberSelect/views/ListItemView.js
@@ -39,7 +39,7 @@ export default Marionette.ItemView.extend({
     },
 
     __getText: function () {
-        return this.model.get('fullName') || this.model.get('name');
+        return this.model.get('name') || this.model.get('userName');
     },
 
     onHighlighted: function (fragment)
