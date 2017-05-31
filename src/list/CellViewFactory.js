ce3278490479a8720b882155bd6f0dd68a72e0f2
Ksenia Kartvelishvili <Ksenia.Kartvelishvili@comindware.com>
2017-05-31 19:34:52 +0300

    refactor BooleanCellView

--- src/list/CellViewFactory.js
+++ src/list/CellViewFactory.js
@@ -80,8 +80,8 @@ let factory = {
 
     getBooleanCellView: function () {
         return factory.__getSimpleView(
-            '{{#if value}}<svg class="svg-icons svg-icons_flag-yes"><use xlink:href="#icon-checked"></use></svg>{{/if}}' +
-            '{{#unless value}}<svg class="svg-icons svg-icons_flag-none"><use xlink:href="#icon-remove"></use></svg>{{/unless}}');
+            '{{#if value}}<svg class="svg-grid-icons svg-icons_flag-yes"><use xlink:href="#icon-checked"></use></svg>{{/if}}' +
+            '{{#unless value}}<svg class="svg-grid-icons svg-icons_flag-none"><use xlink:href="#icon-remove"></use></svg>{{/unless}}');
     },
 
     getDateTimeCellView: function () {
