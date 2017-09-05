1f3849471b4d4c6a8defa231ed9fc2010c7515ec
Ivan Zaycev <Ivan.Zaycev@comindware.com>
2017-09-05 12:33:56 +0300

    add title for collection reference and text cells

--- src/form/editors/TextEditorView.js
+++ src/form/editors/TextEditorView.js
@@ -81,7 +81,9 @@ formRepository.editors.Text = BaseItemEditorView.extend(/** @lends module:core.f
     template: Handlebars.compile(template),
 
     templateHelpers() {
-        return this.options;
+        return _.extend(this.options, {
+            title: this.model.get('value') ? this.model.get('value') : ''
+        });
     },
 
     events: {
