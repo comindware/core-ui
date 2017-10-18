bf89273c9597d34abc185dc9b643d26b1fc17bb8
Vladislav Smirnov <vdmsmirnov@gmail.com>
2017-10-18 23:46:05 +0300

    Native grid item view behavior: optimization v1

--- src/nativeGrid/views/behaviors/NativeGridItemViewBehavior.js
+++ src/nativeGrid/views/behaviors/NativeGridItemViewBehavior.js
@@ -7,11 +7,35 @@
  */
 
 import 'lib';
-import GridItemViewBehavior from '../../../list/views/behaviors/GridItemViewBehavior';
+import { helpers, htmlHelpers } from 'utils';
 
-const NativeGridItemViewBehavior = GridItemViewBehavior.extend({
+const eventBubblingIgnoreList = [
+    'before:render',
+    'render',
+    'dom:refresh',
+    'before:show',
+    'show',
+    'before:destroy',
+    'destroy'
+];
+
+const NativeGridItemViewBehavior = Marionette.Behavior.extend({
     initialize(options, view) {
-        GridItemViewBehavior.prototype.initialize.apply(this, arguments);
+      helpers.ensureOption(view.options, 'columns');
+      helpers.ensureOption(view.options, 'gridEventAggregator');
+      helpers.ensureOption(view.options, 'internalListViewReqres');
+      helpers.ensureOption(options, 'padding');
+
+      this.padding = options.padding;
+      this.listenTo(view.options.gridEventAggregator, 'columnsResize', this.__handleColumnsResize);
+      this.columns = view.options.columns;
+
+      this.listenTo(view, 'all', eventName => {
+          if (eventBubblingIgnoreList.indexOf(eventName) !== -1) {
+              return;
+          }
+          view.options.internalListViewReqres.request('childViewEvent', view, eventName, _.rest(arguments, 1));
+      });
 
         this.paddingLeft = view.options.paddingLeft;
         this.paddingRight = view.options.paddingRight;
@@ -21,6 +45,21 @@ const NativeGridItemViewBehavior = GridItemViewBehavior.extend({
         this.listenTo(view.options.gridEventAggregator, 'singleColumnResize', this.__onSingleColumnResize);
         this.view.setFitToView = this.setFitToView.bind(this);
     },
+
+        modelEvents: {
+            selected: '__handleSelection',
+            deselected: '__handleDeselection',
+            highlighted: '__handleHighlighting',
+            unhighlighted: '__handleUnhighlighting'
+        },
+
+        events: {
+            mousedown: '__handleClick'
+        },
+
+        ui: {
+            cells: '.js-grid-cell'
+        },
 
     __onColumnStartDrag(index) {
         const cells = this.__getCellElements();
@@ -35,23 +74,29 @@ const NativeGridItemViewBehavior = GridItemViewBehavior.extend({
         delete this.draggedColumn;
     },
 
-    onShow() {
-        this.__setInitialWidth(true);
-    },
-
     setFitToView() {
         this.__setInitialWidth();
     },
 
+        onRender() {
+            const model = this.view.model;
+            if (model.selected) {
+                this.__handleSelection();
+            }
+            if (model.highlighted) {
+                this.__highlight(model.highlightedFragment);
+            }
+            if (htmlHelpers.isElementInDom(this.el)) {
+                Marionette.triggerMethodOn(this.view, 'show');
+            }
+
+            this.__setInitialWidth();
+        },
+
     __setInitialWidth() {
-        const $cells = this.__getCellElements();
-
-        for (let i = 0; i < $cells.length; i++) {
-            const $cell = $($cells[i]);
-            const cellWidth = this.columns[i].width;
-
-            $cell.outerWidth(cellWidth);
-        }
+        this.__getCellElements().forEach((cell, i) => {
+          cell.css('width', this.columns[i].width);
+        });
     },
 
     __getElementOuterWidth(el) {
@@ -59,7 +104,43 @@ const NativeGridItemViewBehavior = GridItemViewBehavior.extend({
     },
 
     __onSingleColumnResize(newColumnWidth) {
-        this.gridCellDragger.outerWidth(newColumnWidth);
+        this.gridCellDragger.css('width', newColumnWidth);
+    },
+
+    __getAvailableWidth() {
+        return this.el.css('wdith') - this.padding - 1; //Magic cross browser pixel, don't remove it
+    },
+
+    __getCellElements() {
+        return this.$el.find('.js-grid-cell');
+    },
+
+    __handleClick(e) {
+        const model = this.view.model;
+        const selectFn = model.collection.selectSmart || model.collection.select;
+        if (selectFn) {
+            selectFn.call(model.collection, model, e.ctrlKey, e.shiftKey);
+        }
+    },
+
+    __handleHighlighting(sender, e) {
+        this.__highlight(e.text);
+    },
+
+    __highlight(fragment) {
+        this.view.onHighlighted(fragment);
+    },
+
+    __handleUnhighlighting() {
+        this.view.onUnhighlighted();
+    },
+
+    __handleSelection() {
+        this.$el.addClass('selected');
+    },
+
+    __handleDeselection() {
+        this.$el.removeClass('selected');
     }
 });
 
