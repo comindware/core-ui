8689728a82e4b78fc69665bcf5aef9c9e612549e
Vladislav Smirnov <Vladislav.Smirnov@comindware.com>
2018-03-16 14:34:09 +0300

    Reference bubble editor: update for simple values

--- src/form/editors/impl/reference/models/DefaultReferenceModel.js
+++ src/form/editors/impl/reference/models/DefaultReferenceModel.js
@@ -11,5 +11,11 @@ import list from 'list';
 export default Backbone.Model.extend({
     initialize() {
         _.extend(this, new list.models.behaviors.ListItemBehavior(this));
+    },
+
+    parse(data) {
+        if (typeof data === 'string') {
+            return { name: data, id: data };
+        }
     }
 });
