8dff3ede5256d570eff84a3fbcc42a5b83cb3782
Vladislav Smirnov <Vladislav.Smirnov@comindware.com>
2018-06-24 18:26:24 +0300

    Return of the service worker.

--- demo/public/index.js
+++ demo/public/index.js
@@ -20,3 +20,16 @@ Application.appRouter = new AppRouter({
 const app = new Application();
 window.app = app;
 app.start();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(registration => {
+                console.log('SW registered: ', registration);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
