c014b441ecb88f3b95c09a257cecf0cc5716d77f
Vladislav Smirnov <vs@comindware.com>
2018-09-14 15:43:38 +0300

    Add custom filter panel. Update comparators tests.

---
+++ src/list/views/filterPanel/GridColumnConfigTabPanel.js
@@ -0,0 +1,62 @@
+import template from '../../templates/gridColumnConfigTabPanel.html';
+
+const constants = {
+    animationTime: 100,
+    contentRegion: '.js-content-region'
+};
+
+export default Marionette.View.extend({
+    initialize(options) {
+        this.isOpen = false;
+        this.panelView = this.__createPanelView(options.panelView, options.panelViewOptions);
+    },
+
+    template: Handlebars.compile(template),
+
+    className: 'columns-filters js-columns-filters dev-columns-filters',
+
+    events: {
+        mouseleave: '__handleMouseLeave'
+    },
+
+    triggers: {
+        mouseenter: 'mouseenter'
+    },
+
+    regions: {
+        tabPanelRegion: '.js-tab-panel-region'
+    },
+
+    onRender() {
+        this.showChildView('tabPanelRegion', this.panelView);
+        this.listenTo(Core.services.GlobalEventService, 'window:mousedown:captured', this.__handleGlobalMousedown);
+    },
+
+    __handleGlobalMousedown(target) {
+        if (!this.__isNestedInPanel(target) && !this.__isNestedInButton(target)) {
+            this.trigger('mouseDown', true);
+        }
+    },
+
+    __isNestedInButton(testedEl) {
+        return this.el === testedEl || $.contains(this.el, testedEl) || $.contains(this.panelView.el, testedEl);
+    },
+
+    __isNestedInPanel(testedEl) {
+        return !!$(testedEl).parents('.js-core-ui__global-popup-region').length || !!$(testedEl).parents('.js-grid-header-region').length;
+    },
+
+    updatePanelView(options) {
+        this.panelView.updateView && this.panelView.updateView(options);
+    },
+
+    __createPanelView(panelView, options) {
+        return new panelView(options);
+    },
+
+    __handleMouseLeave(event) {
+        if ($(event.toElement).parents(constants.contentRegion).length > 0) {
+            this.trigger('mouseleave');
+        }
+    }
+});
