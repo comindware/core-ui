a84e16a74428fac1b8071ef8dabebfe338dbf557
Pavel Koroteev <Pavel.koroteev@comindware.com>
2018-09-28 15:01:57 +0300

    DateTime: display seconds depends format. add _.defaultsPure.

--- src/utils/underscore.js
+++ src/utils/underscore.js
@@ -31,5 +31,9 @@ export default {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
+    },
+
+    defaultsPure: function(...args){
+        return Object.assign({}, ...(args.reverse()));
     }
 };
