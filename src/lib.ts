3ef3e530df4f82d4c2625a464fe1ba574ed49c3e
Vladislav Smirnov <vs@comindware.com>
2018-10-23 21:39:47 +0300

    Update typings.

---
+++ src/lib.ts
@@ -0,0 +1,68 @@
+/* Data & Datatime utils*/
+
+// @ts-ignore
+import moment_ from 'moment-timezone';
+import '../node_modules/moment-timezone/moment-timezone-utils';
+import 'moment/locale/ru';
+import 'moment/locale/en-gb';
+import 'moment/locale/de';
+
+/* Text Mask */
+// @ts-ignore
+import maskInput from 'vanilla-text-mask';
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import emailMask from 'text-mask-addons/dist/emailMask';
+
+/* --- */
+import * as _underscore from 'underscore';
+
+import * as mixin from './utils/underscore';
+/* Core.Model utils */
+import backbone from 'backbone';
+import * as Marionette_ from 'backbone.marionette';
+import 'backbone.modelbinder';
+import 'backbone-computedfields';
+import 'backbone.radio';
+import 'backbone-associations';
+/* --- */
+import 'jstorage';
+import * as Handlebars_ from 'handlebars';
+
+import * as $_ from 'jquery';
+// @ts-ignore
+import autosize from 'autosize';
+
+import CodeMirror from 'codemirror';
+import 'innersvg-polyfill';
+// @ts-ignore
+import * as jsencrypt from 'jsencrypt';
+// @ts-ignore
+import * as jqui from 'jquery-ui';
+
+(<any>window)._ = _underscore.mixin(mixin.default);
+
+const api = {
+    'jquery-ui': jqui,
+    moment: moment_,
+    Handlebars: Handlebars_,
+    $: $_,
+    Backbone: backbone,
+    Marionette: Marionette_,
+    codemirror: CodeMirror,
+    JSEncrypt: jsencrypt.JSEncrypt,
+    autosize,
+    maskInput,
+    createNumberMask,
+    emailMask
+};
+
+const moment = api.moment;
+const Handlebars = api.Handlebars;
+const $ = api.$;
+const Backbone = backbone;
+const Marionette = Marionette_;
+const codemirror = api.codemirror;
+const _ = (<any>window)._;
+
+export default api;
+export { _, moment, Handlebars, $, Backbone, Marionette, codemirror, autosize, createNumberMask, maskInput, emailMask };
--- src/utils/underscore.ts
+++ src/utils/underscore.ts
@@ -1,13 +1,17 @@
+import { Model } from 'backbone';
+declare var _: any;
+
 export default {
-    cloneDeep(obj) {
-        let out;
+    cloneDeep(obj: Array<any> | object | Model): Array<any> | object {
+        let out: Array<any> | object;
         let i;
-        const pureJSType = obj && obj.toJSON ? obj.toJSON() : obj; //converting Backbone to js
+        const pureJSType = obj instanceof Model ? obj.toJSON() : obj; //converting Backbone to js
 
         if (Array.isArray(pureJSType)) {
             out = [];
             for (i = pureJSType.length; i; ) {
                 --i;
+                // @ts-ignore
                 out[i] = _.cloneDeep(pureJSType[i]);
             }
 
@@ -16,7 +20,8 @@ export default {
 
         if (_.isObject(pureJSType) && typeof pureJSType !== 'function') {
             out = {};
-            _.map(pureJSType, (value, key) => {
+            _.map(pureJSType, (value: any, key: string) => {
+                // @ts-ignore
                 out[key] = _.cloneDeep(value);
             });
 
@@ -35,7 +40,7 @@ export default {
         });
     },
 
-    defaultsPure(...args) {
+    defaultsPure(...args: Array<any>) {
         return Object.assign({}, ...args.reverse());
     }
 };
