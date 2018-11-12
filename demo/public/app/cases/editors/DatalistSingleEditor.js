a1e9cd97dd0cb4d7de69df0cdca06ab94fe6048c
Pavel Koroteev <Pavel.koroteev@comindware.com>
2018-11-12 13:32:44 +0300

    Datalists: selected item not set on first click. Add 2 cases to editors.

---
+++ demo/public/app/cases/editors/DatalistSingleEditor.js
@@ -0,0 +1,27 @@
+
+
+import CanvasView from 'demoPage/views/CanvasView';
+
+export default function() {
+    const model = new Backbone.Model({
+        DatalistValue: [{
+            id: 'task.1',
+            text: 'Test Reference 1'
+        }]
+    });
+
+    return new CanvasView({
+        view: new Core.form.editors.DatalistEditor({
+            model,
+            key: 'DatalistValue',
+            autocommit: true,
+            showEditButton: true,
+            showAddNewButton: true,
+            showCheckboxes: true,
+            maxQuantitySelected: 1,
+            controller: new Core.form.editors.reference.controllers.DemoReferenceEditorController()
+        }),
+        presentation: "[ {{#each DatalistValue}}<div>{ id: '{{this.id}}', text: '{{this.text}}' }{{#unless @last}}, {{/unless}}</div>{{/each}} <div>]</div>",
+        isEditor: true
+    });
+}
