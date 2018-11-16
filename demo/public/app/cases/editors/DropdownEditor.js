6b0b78f5aeb3adc3c59beb415693ae5f0fc29e5d
Pavel Koroteev <Pavel.koroteev@comindware.com>
2018-11-16 16:26:55 +0300

    Datalist: fix context edi

--- demo/public/app/cases/editors/DropdownEditor.js
+++ demo/public/app/cases/editors/DropdownEditor.js
@@ -4,7 +4,8 @@ export default function() {
     const possibleItems = _.times(200, n => ({
         id: n,
         text: `Text ${n}`,
-        subtext: `subtext ${n}`
+        subtext: `subtext ${n}`,
+        contextIcon: n % 2 ? 'decimal' : 'text'
     }));
 
     const model = new Backbone.Model({
@@ -17,6 +18,9 @@ export default function() {
         autocommit: true,
         collection: possibleItems,
         valueType: 'id',
+        showAdditionalList: true,
+        iconProperty: 'contextIcon',
+        subtextProperty: 'subtext',
         allowEmptyValue: false
     });
 
