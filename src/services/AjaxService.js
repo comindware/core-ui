7a9abe70ae401b698d1c71573f74d73ae7036092
Vladislav Smirnov <vs@comindware.com>
2018-12-07 15:09:20 +0300

    Fix unhandledrejection event in IE.

--- src/services/AjaxService.js
+++ src/services/AjaxService.js
@@ -134,7 +134,7 @@ export default (window.Ajax = new (Marionette.Object.extend({
                 if (window.onunhandledrejection === undefined) {
                     if (MobileService.isIE) {
                         const unhandledRejectionEvent = document.createEvent('Event');
-                        unhandledRejectionEvent.initEvent('submit', false, true);
+                        unhandledRejectionEvent.initEvent('unhandledrejection', false, true);
                         Object.assign(unhandledRejectionEvent, error);
                         window.dispatchEvent(unhandledRejectionEvent);
                     } else {
