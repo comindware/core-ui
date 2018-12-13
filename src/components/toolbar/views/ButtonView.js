f5381a84d5dd3522a3db8402881f6a132d921244
Vasiliy Zorin <Vasiliy.Zorin@comindware.com>
2018-12-13 13:48:44 +0300

    add abilty to set 'title' attribute to buttons

--- src/components/toolbar/views/ButtonView.js
+++ src/components/toolbar/views/ButtonView.js
@@ -8,8 +8,8 @@ function unCapitalizeFirstLetter(string) {
 export default Marionette.View.extend({
     template: Handlebars.compile(template),
 
-    attributes: {
-        tabindex: 0
+    attributes() {
+        return Object.assign({ tabindex: 0 }, this.model.get('title') ? { title: this.model.get('title') } : {});
     },
 
     className() {
