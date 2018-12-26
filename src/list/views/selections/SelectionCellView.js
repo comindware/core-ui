b2584ecb82cdd32d4ad1a8cb1f75c221190c1f77
Vladislav Smirnov <vs@comindware.com>
2018-12-26 13:15:40 +0300

    Update events.

--- src/list/views/selections/SelectionCellView.js
+++ src/list/views/selections/SelectionCellView.js
@@ -60,35 +60,6 @@ export default Marionette.View.extend({
         index: '.js-index'
     },
 
-    events: {
-        'click @ui.checkbox': '__handleCheckboxClick',
-        'dragstart @ui.dots': '__handleDragStart',
-        'dragend @ui.dots': '__handleDragEnd',
-        click: '__handleClick',
-        dblclick: '__handleDblClick',
-        dragover: '__handleDragOver',
-        dragenter: '__handleDragEnter',
-        dragleave: '__handleDragLeave',
-        drop: '__handleDrop',
-        mouseenter: '__handleMouseEnter',
-        mouseleave: '__handleMouseLeave',
-        contextmenu: '__handleContextMenu'
-    },
-
-    modelEvents: {
-        'update:model': '__updateIndex',
-        selected: '__handleSelection',
-        deselected: '__handleDeselection',
-        dragover: '__handleModelDragOver',
-        dragleave: '__handleModelDragLeave',
-        drop: '__handleModelDrop',
-        mouseenter: '__handleModelMouseEnter',
-        mouseleave: '__handleModelMouseLeave',
-        blink: '__blink',
-        checked: '__addCheckedClass',
-        unchecked: '__removeCheckedClass'
-    },
-
     onRender() {
         this.__updateState();
 
--- src/list/views/selections/SelectionPanelView.js
+++ src/list/views/selections/SelectionPanelView.js
@@ -13,6 +13,41 @@ export default Marionette.CollectionView.extend({
 
     childView: SelectionCellView,
 
+    ui: {
+        checkbox: '.js-checkbox',
+        dots: '.js-dots',
+        index: '.js-index'
+    },
+
+    events: {
+        'click @ui.checkbox': '__handleCheckboxClick',
+        'dragstart @ui.dots': '__handleDragStart',
+        'dragend @ui.dots': '__handleDragEnd',
+        click: '__handleClick',
+        dblclick: '__handleDblClick',
+        dragover: '__handleDragOver',
+        dragenter: '__handleDragEnter',
+        dragleave: '__handleDragLeave',
+        drop: '__handleDrop',
+        mouseenter: '__handleMouseEnter',
+        mouseleave: '__handleMouseLeave',
+        contextmenu: '__handleContextMenu'
+    },
+
+    modelEvents: {
+        'update:model': '__updateIndex',
+        selected: '__handleSelection',
+        deselected: '__handleDeselection',
+        dragover: '__handleModelDragOver',
+        dragleave: '__handleModelDragLeave',
+        drop: '__handleModelDrop',
+        mouseenter: '__handleModelMouseEnter',
+        mouseleave: '__handleModelMouseLeave',
+        blink: '__blink',
+        checked: '__addCheckedClass',
+        unchecked: '__removeCheckedClass'
+    },
+
     _showCollection() {
         const models = this.collection.visibleModels;
 
