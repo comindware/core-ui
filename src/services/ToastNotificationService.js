bc84460cdd2781c1aa5a9ce0eea265852e4bb7ea
Vladislav Smirnov <vs@comindware.com>
2019-01-10 13:29:14 +0300

    Fix fir toast notifications.

--- src/services/ToastNotificationService.js
+++ src/services/ToastNotificationService.js
@@ -20,7 +20,7 @@ export default class ToastNotificationService {
         Object.assign(this, Backbone.Events);
     }
 
-    static add(message, type = this.notificationTypes.SUCCESS, options) {
+    static add(message, type = this.notificationTypes.SUCCESS, options = {}) {
         if (!message) {
             return;
         }
