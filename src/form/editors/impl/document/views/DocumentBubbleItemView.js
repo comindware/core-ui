6075ff3f5517dedcdbc9561bdc5ec878c68ec858
Stanislav Guryev <Stanislav.Guryev@comindware.com>
2019-01-25 13:08:48 +0300

    Downloading just uploaded docuemtns

--- src/form/editors/impl/document/views/DocumentBubbleItemView.js
+++ src/form/editors/impl/document/views/DocumentBubbleItemView.js
@@ -89,14 +89,20 @@ export default Marionette.View.extend({
     },
 
     __showPreview() {
-        return this.attachmentsController.showGallery(this.model);
+        const result = this.attachmentsController.showGallery(this.model);
+        if (this.__isNew() && window.navigator.msSaveOrOpenBlob && result) {
+            window.navigator.msSaveOrOpenBlob(this.model.get('file'), this.model.get('name'));
+            return false;
+        }
+
+        return result;
     },
 
     __onMouseenter() {
         if (this.options.allowDelete) {
             this.el.insertAdjacentHTML('beforeend', iconWrapRemoveBubble);
         }
-        if (this.model.id?.indexOf(savedDocumentPrefix) > -1 && this.options.showRevision) {
+        if (!this.__isNew() && this.options.showRevision) {
             if (!this.isRevisonButtonShown) {
                 this.documentRevisionPopout = new dropdown.factory.createDropdown({
                     buttonView: DocumentRevisionButtonView,
@@ -113,6 +119,10 @@ export default Marionette.View.extend({
                 this.ui.revise.show();
             }
         }
+    },
+
+    __isNew() {
+        return !this.model.id?.startsWith(savedDocumentPrefix);
     },
 
     __onMouseleave() {
