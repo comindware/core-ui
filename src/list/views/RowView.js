6af5e0d2490272f92aca5992d6f2f7caa2f69a32
Vladislav Smirnov <vs@comindware.com>
2019-02-14 09:35:05 +0300

    Small grid refactoring. Update demo page design.

--- src/list/views/RowView.js
+++ src/list/views/RowView.js
@@ -142,7 +142,7 @@ export default Marionette.View.extend({
         }
 
         const isTree = this.getOption('isTree');
-        this.options.columns.forEach((gridColumn, index) => {
+        this.options.columns.forEach(gridColumn => {
             const cell = gridColumn.cellView || CellViewFactory.getCellViewForColumn(gridColumn, this.model); // move to factory
 
             if (typeof cell === 'string') {
@@ -320,10 +320,10 @@ export default Marionette.View.extend({
 </svg>`
                 : ''
         }${
-                this.options.index
+                this.options.showRowIndex
                     ? `
 <span class="js-index cell__index">
-    {{index}}
+    ${this.model.collection.indexOf(this.model) + 1}
 </span>`
                     : ''
             }
