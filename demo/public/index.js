6aa3f3ad811cdbec92f11ac7ebd75f9655121675
Pavel Koroteev <Pavel.koroteev@comindware.com>
2019-02-14 17:33:09 +0300

    fix demo fas.

--- demo/public/index.js
+++ demo/public/index.js
@@ -1,3 +1,6 @@
+import 'styles/defaultFontawesome.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/solid.css';
 import '../../dist/core.css';
 import 'styles/demo.css';
 import core from 'comindware/core';
