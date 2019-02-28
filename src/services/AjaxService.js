3459452674e83102488a6f9ad473326855b03222
Vladislav Smirnov <vs@comindware.com>
2019-02-28 14:23:13 +0300

    Update fetch configuration.

--- src/services/AjaxService.js
+++ src/services/AjaxService.js
@@ -48,8 +48,9 @@ export default (window.Ajax = new (Marionette.MnObject.extend({
                 method,
                 body: body ? JSON.stringify(body) : null,
                 traditional: true,
-                dataType: 'json',
-                contentType: 'application/json'
+                headers: {
+                    'Content-type': 'application/json'
+                }
             },
             options || {}
         );
@@ -70,6 +71,9 @@ export default (window.Ajax = new (Marionette.MnObject.extend({
             fetch(url, {
                 method: 'POST',
                 body: formData,
+                headers: {
+                    'Content-type': 'application/json'
+                },
                 processData: false,
                 contentType: false
             })
