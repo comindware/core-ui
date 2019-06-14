b78d8acfe81c136bf438310528b6ff5493d08c2b
Vasiliy Zorin <Vasiliy.Zorin@comindware.com>
2019-06-14 16:37:53 +0300

    fix styles

--- src/components/treeEditor/views/TEButtonView.ts
+++ src/components/treeEditor/views/TEButtonView.ts
@@ -3,6 +3,8 @@ import template from '../templates/TEButton.hbs';
 export default Marionette.View.extend({
     template: Handlebars.compile(template),
 
+    className: 'tree-editor-btn',
+
     templateContext() {
         return {
             iconClass: this.options.iconClass
