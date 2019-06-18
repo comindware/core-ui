77639bc59c4dd675d1378f0cfec4c7c29d93f28c
Vasiliy Zorin <Vasiliy.Zorin@comindware.com>
2019-06-18 16:38:32 +0300

    small refactor

--- src/components/treeEditor/views/LeafView.ts
+++ src/components/treeEditor/views/LeafView.ts
@@ -1,15 +1,12 @@
 import template from '../templates/leaf.hbs';
 import NodeViewConfig from '../services/NodeViewConfig';
 
-export default Marionette.View.extend(
-    Object.assign(
-        {
-            attributes: {
-                draggable: 'true'
-            },
+export default Marionette.View.extend({
+    attributes: {
+        draggable: 'true'
+    },
 
-            className: 'leaf-item tree-item'
-        },
-        NodeViewConfig(template)
-    )
-);
+    className: 'leaf-item tree-item',
+
+    ...NodeViewConfig(template)
+});
