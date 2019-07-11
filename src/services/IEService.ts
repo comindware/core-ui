50fb1ec5b43bda578fd88b01371f857b7d3f9d8a
Vasiliy Zorin <Vasiliy.Zorin@comindware.com>
2019-07-11 11:53:00 +0300

    add classList.add multiple arguments support in IE

--- src/services/IEService.ts
+++ src/services/IEService.ts
@@ -2,8 +2,22 @@ import 'innersvg-polyfill';
 
 export default class IEService {
     static initialize() {
-        DOMTokenList.prototype.toggle = function (name, flag = !this.contains(name)) {
+        DOMTokenList.prototype.toggle = function(name, flag = !this.contains(name)) {
             return flag ? (this.add(name), true) : (this.remove(name), false);
+        };
+
+        const oldAdd = DOMTokenList.prototype.add;
+        DOMTokenList.prototype.add = function(className) {
+            return arguments.length > 1 
+            ? ([...arguments].map(name => oldAdd.call(this, name)), true) 
+            : (oldAdd.call(this, className), true);
+        };
+
+        const oldRemove = DOMTokenList.prototype.add;
+        DOMTokenList.prototype.remove = function(className) {
+            return arguments.length > 1 
+            ? ([...arguments].map(name => oldRemove.call(this, name)), false) 
+            : (oldRemove.call(this, className), false);
         };
     }
 }
