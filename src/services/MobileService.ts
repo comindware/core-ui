25a737771a31ae824aba831b9ebb5f69c953ad19
Elizaveta Salova <Elizaveta.Salova@comindware.com>
2019-07-25 18:51:22 +0300

    [#449683] Add isPhone & isTablet, refactoring

--- src/services/MobileService.ts
+++ src/services/MobileService.ts
@@ -2,38 +2,18 @@ import IEService from './IEService';
 import EdgeService from './EdgeService';
 
 export default class MobileService {
-    static isMobile: boolean;
-    static isIE: boolean;
-    static isEdge: boolean;
     static initialize() {
-        if (
-            navigator.userAgent.match(/Android/i) ||
-            navigator.userAgent.match(/webOS/i) ||
-            navigator.userAgent.match(/iPhone/i) ||
-            navigator.userAgent.match(/iPad/i) ||
-            navigator.userAgent.match(/iPod/i) ||
-            navigator.userAgent.match(/BlackBerry/i) ||
-            navigator.userAgent.match(/Windows Phone/i)
-        ) {
-            this.isMobile = true;
-        } else {
-            this.isMobile = false;
-        }
-        if (
-            navigator.appName === 'Microsoft Internet Explorer' ||
-            !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) ||
-            (typeof $.browser !== 'undefined' && $.browser.msie === 1)
-        ) {
-            this.isIE = true;
-            IEService.initialize();
-        } else {
-            this.isIE = false;
-        }
-        if (/Edge\//.test(navigator.userAgent)) {
-            this.isEdge = true;
-            EdgeService.initialize();
-        } else {
-            this.isEdge = false;
-        }
+        const userAgent = navigator.userAgent;
+        const isPhone = /Android|webOS|iPhone|iPod|Blackberry|Windows Phone/i.test(userAgent)
+            && window.innerWidth <= 414 
+            && window.innerHeight <= 896;
+        const isTablet = /iPad|Android/i.test(userAgent) && window.innerWidth <= 1280 && window.innerHeight <= 1366;
+        const isMobile = (isPhone || isTablet) != null;
+
+        const isIE = navigator.appName === 'Microsoft Internet Explorer' || /MSIE|Trident/i.test(userAgent);
+        isIE && IEService.initialize();
+
+        const isEdge = /Edge\//.test(userAgent);
+        isEdge && EdgeService.initialize();
     }
 }
