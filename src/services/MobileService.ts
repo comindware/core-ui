87509f0292a33f7de9a0b5daa0de87c4f55b1549
Elizaveta Salova <Elizaveta.Salova@comindware.com>
2019-07-25 20:12:53 +0300

    [#449683] Refactoring

--- src/services/MobileService.ts
+++ src/services/MobileService.ts
@@ -2,18 +2,23 @@ import IEService from './IEService';
 import EdgeService from './EdgeService';
 
 export default class MobileService {
+    static isEdge: boolean;
+    static isIE: boolean;
+    static isMobile: boolean;
+    static isPhone: boolean;
+    static isTablet: boolean;
     static initialize() {
         const userAgent = navigator.userAgent;
-        const isPhone = /Android|webOS|iPhone|iPod|Blackberry|Windows Phone/i.test(userAgent)
+        this.isPhone = /Android|webOS|iPhone|iPod|Blackberry|Windows Phone/i.test(userAgent)
             && window.innerWidth <= 414 
             && window.innerHeight <= 896;
-        const isTablet = /iPad|Android/i.test(userAgent) && window.innerWidth <= 1280 && window.innerHeight <= 1366;
-        const isMobile = (isPhone || isTablet) != null;
+        this.isTablet = /iPad|Android/i.test(userAgent) && window.innerWidth <= 1280 && window.innerHeight <= 1366;
+        this.isMobile = this.isPhone || this.isTablet;
 
-        const isIE = navigator.appName === 'Microsoft Internet Explorer' || /MSIE|Trident/i.test(userAgent);
-        isIE && IEService.initialize();
+        this.isIE = navigator.appName === 'Microsoft Internet Explorer' || /MSIE|Trident/i.test(userAgent);
+        this.isIE && IEService.initialize();
 
-        const isEdge = /Edge\//.test(userAgent);
-        isEdge && EdgeService.initialize();
+        this.isEdge = /Edge\//.test(userAgent);
+        this.isEdge && EdgeService.initialize();
     }
 }
