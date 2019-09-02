2a21a3b008735ffc45244a963fb87ded5d5af459
Stanislav Guryev <Stanislav.Guryev@comindware.com>
2019-09-02 18:49:52 +0300

    Cherry pick from 1.13

--- src/dropdown/views/DropdownView.ts
+++ src/dropdown/views/DropdownView.ts
@@ -109,9 +109,8 @@ export default class DropdownView {
         this.button.once('render', () => {
             this.isShown = true;
             this.button.on('change:content', () => this.__adjustPosition(true));
-            if (options.autoOpen !== false) {
-                this.button.el.addEventListener('click', this.__handleClick.bind(this));
-            }
+            this.button.on('toggle', () => this.toggle());
+            this.button.el.addEventListener('click', this.__handleClick.bind(this));
             this.button.el.addEventListener('blur', this.__onBlur.bind(this));
 
             if (!this.options.customAnchor && this.options.showDropdownAnchor) {
@@ -363,10 +362,11 @@ export default class DropdownView {
         }
     }
 
-    __handleClick() {
+    __handleClick(...args) {
         if (this.options.autoOpen) {
             this.toggle();
         }
+        this.button.trigger('click', ...args);
     }
 
     __isNestedInButton(testedEl) {
