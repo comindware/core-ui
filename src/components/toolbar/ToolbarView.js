4c82fd1ed3dd1a2e204aa00bdbf0d9ddc1c8f9e7
pkoroteev <Pavel.Koroteev@comindware.com>
2019-09-18 11:50:03 +0300

    Toolbar: add viewComparator by order property, fix infinite rebuild.

--- src/components/toolbar/ToolbarView.js
+++ src/components/toolbar/ToolbarView.js
@@ -134,6 +134,7 @@ export default Marionette.View.extend({
         const allCollapsibleModels = this.groupedCollection.getCollapsibleModels();
 
         if (allCollapsibleModels.length === 0) {
+            clearInterval(interval);
             return;
         }
 
