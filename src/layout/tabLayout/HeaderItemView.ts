4c6e7dd7be9b360028716c1bad2f5aa8baaa460e
pkoroteev <Pavel.Koroteev@comindware.com>
2019-11-22 15:59:25 +0300

    TabLayout: added ellispsis if tabs overflow (merge from 1.13).

---
+++ src/layout/tabLayout/HeaderItemView.ts
@@ -0,0 +1,74 @@
+// @flow
+import template from './templates/headerItem.hbs';
+
+export default Marionette.View.extend({
+    tagName: 'li',
+
+    className() {
+        return `layout__tab-layout__header-view-item ${this.model.get('tabClass') || ''}`;
+    },
+
+    attributes() {
+        return { ...(this.model?.get('description') ? { title: this.model.get('description') } : {}) };
+    },
+
+    template: Handlebars.compile(template),
+
+    events: {
+        click: '__onClick'
+    },
+
+    modelEvents: {
+        'change:selected change:error change:enabled change:visible': 'render',
+        'change:selected': '__onSelectedChange'
+    },
+
+    onRender() {
+        this.$el.toggleClass('layout__tab-layout__header-view-item_selected', Boolean(this.model.get('selected')));
+        this.$el.toggleClass('layout__tab-layout__header-view-item_error', Boolean(this.model.get('error')));
+        this.$el.toggleClass('layout__tab-layout__header-view-item_disabled', !this.model.get('enabled'));
+        this.$el.toggleClass('layout__tab-layout__header-view-item_hidden', !this.model.get('visible'));
+
+        this.el.setAttribute('id', this.model.id);
+    },
+
+    __onClick() {
+        if (this.model.get('enabled')) {
+            this.trigger('select', this.model);
+        }
+    },
+
+    __onSelectedChange(model: Backbone.Model, selected: boolean) {
+        if (selected) {
+            this.__scrollIntoViewIfNeeded();
+        }
+    },
+
+    // native scrollIntoView causing the whole page to move,
+    // to prevent that behaviour we need to use scrollIntoViewOptions,
+    // but Edge, IE, and Safari doesn't support scrollIntoViewOptions.
+    __scrollIntoViewIfNeeded() {
+        if (!this.__isUiReady() || this.__isIntoView()) {
+            return;
+        }
+        const offsetLeft = this.el.offsetLeft;
+        this.$el.offsetParent().animate({ scrollLeft: offsetLeft }, 300);
+    },
+
+    
+    __isIntoView() {
+        const parentScrollLeft = this.el.parentElement.scrollLeft;
+        const parentOffsetWidth = this.el.parentElement.offsetWidth;
+        const parentScrollRight = parentScrollLeft + parentOffsetWidth;
+
+        const elOffsetLeft = this.el.offsetLeft;
+        const elOffsetWidth = this.el.offsetWidth;
+        const elOffsetRight = elOffsetLeft + elOffsetWidth;
+
+        return elOffsetLeft > parentScrollLeft && elOffsetRight < parentScrollRight;
+    },
+
+    __isUiReady() {
+        return this.isRendered() && !this.isDestroyed() && this.isAttached();
+    }
+});
