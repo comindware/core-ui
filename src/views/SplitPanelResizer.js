a1017d53ec6c91e9d3eac16f58022fd8e5113dd6
Anna Mitroshenkova <amit@FMLPANDDEV>
2020-04-15 14:45:49 +0300

    [#481406] Fix saving position of splitView

--- src/views/SplitPanelResizer.js
+++ src/views/SplitPanelResizer.js
@@ -25,28 +25,42 @@ export default Marionette.View.extend({
         const rect = this.firstPanel.el.getBoundingClientRect();
         const parentRect = this.firstPanel.parentEl().getBoundingClientRect();
         if (this.options.orientation === splitViewTypes.HORIZONTAL) {
-            const originalPanelHeight = rect.height;
+            let originalPanelHeight = rect.height;
             this.topOffset = rect.top;
             this.originalParentHeight = parentRect.height + parentRect.top;
             this.el.className = 'split-panel-resizer_container split-panel-resizer_horizontal';
+            if (this.options.splitPosition) {
+                originalPanelHeight = Number(this.options.splitPosition);
+            }
             this.__setTopPosition(originalPanelHeight);
 
             Core.services.UIService.draggable({
                 el: this.el,
                 axis: 'y',
-                drag: (event, ui) => this.__onResizerDragHorizontal(ui)
+                drag: (event, ui) => this.__onResizerDragHorizontal(ui),
+                stop: (event, ui) => {
+                    const height = ui.offsetTop - this.topOffset;
+                    this.trigger('change:resizer', height);
+                }
             });
         } else if (this.options.orientation === splitViewTypes.VERTICAL) {
-            const originalPanelWidth = rect.width;
+            let originalPanelWidth = rect.width;
             this.leftOffset = rect.left;
             this.originalParentWidth = parentRect.width;
             this.el.className = 'split-panel-resizer_container split-panel-resizer_vertical';
+            if (this.options.splitPosition) {
+                originalPanelWidth = Number(this.options.splitPosition);
+            }
             this.__setLeftPosition(originalPanelWidth);
 
             Core.services.UIService.draggable({
                 el: this.el,
                 axis: 'x',
-                drag: (event, ui) => this.__onResizerDragVertical(ui)
+                drag: (event, ui) => this.__onResizerDragVertical(ui),
+                stop: (event, ui) => {
+                    const width = ui.offsetLeft - this.leftOffset;
+                    this.trigger('change:resizer', width);
+                }
             });
         } else {
             this.el.className = 'split-panel-resizer_container split-panel-resizer_general';
@@ -76,6 +90,7 @@ export default Marionette.View.extend({
         this.__alignHorizontalSplitter(height);
         Core.services.GlobalEventService.trigger('window:resize');
     },
+
     __handleWindowResize() {
         const rect = this.firstPanel.el.getBoundingClientRect();
         if (this.options.orientation === splitViewTypes.HORIZONTAL) {
