c10a7b29d1b6af0ca1193d83e4783d92838bf256
Pavel Norin <pavelnaureen@hotmail.com>
2021-08-04 20:17:14 +0300

    Add auto layout for showcase

--- src/layout/showcase/ShowcaseView.ts
+++ src/layout/showcase/ShowcaseView.ts
@@ -6,32 +6,45 @@ import Backbone from 'backbone';
 const defaultOptions = {
     groupBy: [3, 6, 10, Infinity],
     startPage: 0,
-    ContentView: ShowcaseContentView
+    ContentView: ShowcaseContentView,
+    toggleUI: true,
+    isAutoFill: true,
+    childWidth: 240
 };
+
+const MIN_MARGIN = 5;
 
 export default Marionette.View.extend({
     initialize() {
         _.defaults(this.options, defaultOptions);
         this.parentCollection = this.options.collection;
         this.currentPage = this.options.startPage;
-        this.currentPaging = this.options.groupBy[0];
         this.collection = new Backbone.Collection();
         const { collection, ...contentViewOptions } = this.options;
         this.contentView = new this.options.ContentView({
             collection: this.collection,
             ...contentViewOptions
         });
-        const groupingModel = new Backbone.Model({ groupBy: this.currentPaging });
+        if (!this.options.isAutoFill) {
+            this.groupByArray = this.options.groupBy;
+            this.currentPaging = this.groupByArray[0];
+            this.__resetGroupByCollection();
+            this.__resetCurrentCollection();
+        } else {
+            const resizeObserver = new ResizeObserver(this.__onResize.bind(this));
+            resizeObserver.observe(this.el);
+        }
+        this.groupingModel = new Backbone.Model({ groupBy: this.currentPaging });
+        this.groupByCollection = new Backbone.Collection();
         this.groupingView = new DatalistEditorView({
             key: 'groupBy',
-            collection: new Backbone.Collection(this.__getGroupByCollection()),
+            collection: this.groupByCollection,
             allowEmptyValue: false,
             autocommit: true,
             valueType: 'id',
-            model: groupingModel
+            model: this.groupingModel
         });
-        this.__resetCurrentCollection();
-        this.listenTo(groupingModel, 'change:groupBy', this.__onChangeGroupBy);
+        this.listenTo(this.groupingModel, 'change:groupBy', this.__onChangeGroupBy);
     },
 
     className: 'showcase-wrapper',
@@ -62,10 +75,33 @@ export default Marionette.View.extend({
         };
     },
 
+    __onResize(entry: Array<ResizeObserverEntry>) {
+        const newContainerWidth = entry[0].contentRect.width;
+        if (newContainerWidth === 0 || Math.abs(newContainerWidth - this.containerWidth) < MIN_MARGIN + this.options.childWidth) {
+            this.containerWidth = newContainerWidth;
+            return;
+        }
+        this.containerWidth = newContainerWidth;
+        this.__countGroupByArray(this.containerWidth);
+        this.currentPaging = this.groupByArray[0];
+        this.__resetGroupByCollection();
+        this.groupingModel.set({ groupBy: this.currentPaging });
+        this.__resetCurrentCollection();
+    },
+
     onRender() {
         this.__inactivateArrows();
         this.showChildView('content', this.contentView);
         this.showChildView('grouping', this.groupingView);
+    },
+
+    __resetGroupByCollection() {
+        this.groupByCollection.reset(this.__getGroupByCollection());
+    },
+
+    __countGroupByArray(containerWidth: number) {
+        const firstEl = Math.floor(containerWidth / (this.options.childWidth + 2 * MIN_MARGIN));
+        this.groupByArray = [firstEl, firstEl * 2, firstEl * 4, Infinity];
     },
 
     __onChangeGroupBy(model: Backbone.Model) {
@@ -76,7 +112,7 @@ export default Marionette.View.extend({
     },
 
     __getGroupByCollection() {
-        return this.options.groupBy.map((n: number) => ({ id: n, name: n === Infinity ? Localizer.get('CORE.LAYOUT.SHOWCASE.SHOWALL') : n }));
+        return this.groupByArray.map((n: number) => ({ id: n, name: n === Infinity ? Localizer.get('CORE.LAYOUT.SHOWCASE.SHOWALL') : n }));
     },
 
     __onLeftArrowClick() {
