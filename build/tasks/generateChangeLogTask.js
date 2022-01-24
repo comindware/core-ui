3d46f3f68ec44c9d2257552c938bca25250f8fb9
Stanislav Guryev <Stanislav.Guryev@comindware.com>
2022-01-24 14:51:16 +0300

    Generate change log fix

--- build/tasks/generateChangeLogTask.js
+++ build/tasks/generateChangeLogTask.js
@@ -4,6 +4,9 @@ const exec = require('child_process').exec;
 const pathResolver = require('../pathResolver');
 
 module.exports = callback => {
+    if (!process.env.PREVIOUTS_PACKAGE_VERSION || !process.env.PACKAGE_VERSION) {
+        callback();
+    }
     exec(`git log --pretty=format:"%s" ${process.env.PREVIOUTS_PACKAGE_VERSION}...${process.env.PACKAGE_VERSION}`, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
@@ -25,7 +28,7 @@ ${stdout.replace(/^Merge pull request.*[\r\n]*/gm, '')}
         `;
         fs.appendFileSync(pathResolver.root('CHANGELOG.md'), template, 'utf8');
 
-        console.log(`CHANGELOG.md has been succecfully generated between versions ${process.env.PREVIOUTS_PACKAGE_VERSION}...${process.env.PACKAGE_VERSION}`);
+        console.log(`CHANGELOG.md has been successfully generated between versions ${process.env.PREVIOUTS_PACKAGE_VERSION}...${process.env.PACKAGE_VERSION}`);
         callback();
     });
 }
